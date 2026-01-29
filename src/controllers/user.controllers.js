d429723e72a357c514422c2d93c6c23b83b2e622
Uday Patil <er.udayp133@gmail.com>
2026-01-29 17:41:51 +0530

    add register controller and bug fixes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,16 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    //get user details from fronted
+    //validation--->  not empty
+    //check if user already exist: username, email
+    //check for image,check for avatar
+    //uploaf them to cloudinary , avatar
+    //create user object -- create entry un db
+    //remove password and refresh token field from response
+    //check for user creation
+    //return res
+
 })
 
 export { registerUser };
